bb01d79b28a90afad005074ca8771b2194745f84
DAVE173234 <frenzdavedacillo@gmail.com>
2026-02-04 11:26:13 +0800

    Display sales summary display

---
+++ js/sales.js
@@ -0,0 +1,6 @@
+// Static values (no database)
+const totalSales = 180;
+const totalOrders = 3;
+
+document.getElementById("sales").textContent = totalSales;
+document.getElementById("orders").textContent = totalOrders;
